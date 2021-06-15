5bb1ec2aa5fd9ec4f2cf4b04846a24c935068cad
Albin Heimerson <albin.heimerson@control.lth.se>
2021-06-15 20:38:28 +0200

    Update period_wait docstring

--- src/AbstractControlProcesses.jl
+++ src/AbstractControlProcesses.jl
@@ -94,14 +94,15 @@ sampletime(p::AbstractProcess) = error("Function not implemented for $(typeof(p)
 
 Should continue execution at start of next period, i.e. when `dt` seconds has passed since `last_time`. 
 
-For a `PhysicalProcess` this is has a default implementation which sleeps from `time()` until 
-`last_time + dt` using `Libc.systemsleep`. Though this is more precise than using the standard 
-`sleep` in Julia, it also blocks the entire Julia thread it is running on so one has to be careful 
-with that sleep.
+For a `PhysicalProcess` this has a default implementation which sleeps until `last_time + dt` using 
+`Libc.systemsleep`. Though this is more precise than using the standard `sleep` in Julia, it also 
+blocks the entire Julia thread it is running on which could affect execution in some cases.
 
-For a `SimulatedProcess` it can either be running in realtime, in which case it should be implemented 
-similarly to the physical process, or it runs at full simulation speed, in which case the environment 
-should simulate a step corresponding to `dt` time when `periodic_wait` is called.
+For a `SimulatedProcess` it can either be running in realtime in a background process, in which case 
+it should be implemented similarly to the physical process, or it runs at full simulation speed, in 
+which case the environment should simulate a step corresponding to `dt` time when `periodic_wait` is called.
+
+A good way to initialize `last_time` to the correct value is to call `last_time = periodic_wait(P, 0.0, 0.0)`.
 """
 periodic_wait(p::AbstractProcess, last_time, dt) = error("Function not implemented for $(typeof(p))")
 function periodic_wait(p::PhysicalProcess, last_time, dt) 
