e6ebb20f2f399a7ed1fb7fc873ab003b2106787d
Albin Heimerson <albin.heimerson@control.lth.se>
2021-06-16 16:38:02 +0200

    Fix Base.finalize

--- src/AbstractControlProcesses.jl
+++ src/AbstractControlProcesses.jl
@@ -144,10 +144,11 @@ initialize(p::AbstractProcess) = error("Function not implemented for $(typeof(p)
 
 """
     finalize(P::AbstractProcess)
+
 This function is called after any control or measurement operations are performed. During a call to `finalize`, one might finalize external communications etc. Before control is done,
 the function [`initialize`](@ref) is called.
 """
-finalize(p::AbstractProcess) = error("Function not implemented for $(typeof(p))")
+Base.finalize(p::AbstractProcess) = error("Function not implemented for $(typeof(p))")
 
 
 end # module
